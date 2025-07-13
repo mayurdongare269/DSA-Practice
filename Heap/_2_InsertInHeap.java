48f6b66254f45030812e6ed0332bfa25c693664e
Mayur Dongare <mayurdongare269@gmail.com>
2025-07-13 18:45:30 +0530

    remove code

--- Heap/_2_InsertInHeap.java
+++ Heap/_2_InsertInHeap.java
@@ -22,6 +22,49 @@ public class _2_InsertInHeap { // here in one code all funcions
         public int peek() {
             return arr.get(0);
         }
+
+        public int remove() { // delete in heap
+            int data = arr.get(0);
+
+            //step1 - swap first and last index
+            int temp = arr.get(0);
+            arr.set(0, arr.size() - 1);
+            arr.set(arr.size() - 1, temp);
+
+            //step2 - remove last
+            arr.remove(arr.size() - 1);
+
+            //step3 - heapify
+            heapify(0);
+            return data;
+        }
+
+        private void heapify(int i) {
+            int left = 2*i+1;
+            int right = 2*i+2;
+            int minIdx = i;
+
+            if(left < arr.size() && arr.get(minIdx) > arr.get(left)) {
+                minIdx = left;
+            }
+
+            if(right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+                minIdx = right;
+            }
+
+            if(minIdx != i) {
+                //swap
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
+
+                heapify(minIdx);
+            }
+        }
+
+
+
+
     }
 
     public static void main(String[] args) {
